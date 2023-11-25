c2da4c44164c8640b64bd662ef26b51d4a5a8a28
frto027 <q602706150@gmail.com>
2023-11-25 15:09:25 +0800

    update some default value

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -9,14 +9,14 @@ DECLARE_CONFIG(ModConfig,
     // Declare "VariableA"
     //CONFIG_VALUE(VariableA, std::string, "Variable Name", "Variable Value");
     CONFIG_VALUE(SelectedBleMac, std::string, "Selected BLE Device Mac", "");
-    CONFIG_VALUE(HeartTextColor, UnityEngine::Color, "The Text Color", UnityEngine::Color(1,1,1,1));
+    CONFIG_VALUE(HeartTextColor, UnityEngine::Color, "The Text Color", UnityEngine::Color(0.08,0.33,0.94,1));
     CONFIG_VALUE(HeartGameCorePos, UnityEngine::Vector3, "Game playing position", UnityEngine::Vector3(3.1,0.2,6.8));
     CONFIG_VALUE(HeartGameCoreRot, float, "Game playing rotate Y", 0);
     CONFIG_VALUE(HeartMainMenuPos, UnityEngine::Vector3, "Main menu position", UnityEngine::Vector3(-2.3,2.8,4));
     CONFIG_VALUE(HeartMainMenuRot, float, "Main menu rotate Y", -30);
     CONFIG_VALUE(HeartLineSpaceDelta, float, "Line Space Delta", -35);
 
-    CONFIG_VALUE(HeartDataComeFlashColor, UnityEngine::Color, "Text Color when Data Flash", UnityEngine::Color(1,0.2,0.2,1));
-    CONFIG_VALUE(HeartDataComeFlashDuration, float, "Speed when Data Flash", 0.4);
+    CONFIG_VALUE(HeartDataComeFlashColor, UnityEngine::Color, "Text Color when Data Flash", UnityEngine::Color(0,1,0.6,1));
+    CONFIG_VALUE(HeartDataComeFlashDuration, float, "Speed when Data Flash", 1);
 
 );
