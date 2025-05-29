972736a3e52d0d3fe6f3a43accb6a8f3e3c10f13
frto027 <q602706150@gmail.com>
2025-05-29 10:32:35 +0800

    enable replay feature for future release

--- src/BeatleaderRecorder.cpp
+++ src/BeatleaderRecorder.cpp
@@ -271,7 +271,7 @@ void Init(){
     }
 
     auto AddReplayCustomDataCallback = CondDeps::FindUnsafe<void, std::string, std::function<void(const char*, size_t)> >("replay", "AddReplayCustomDataCallback");
-    if(false && AddReplayCustomDataCallback.has_value()){
+    if(AddReplayCustomDataCallback.has_value()){
         getLogger().info("Replay mod is detected, enable replay support");
         needReplay = true;
         AddReplayCustomDataCallback.value()("HeartBeatQuest", ReplayCallback);
