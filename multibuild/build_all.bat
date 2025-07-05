cc92bd1d057bccfce08594dfa30b8a7a28a2ad4a
frto027 <q602706150@gmail.com>
2025-07-05 20:49:11 +0800

    update workflow

---
+++ multibuild/build_all.bat
@@ -0,0 +1,3 @@
+python multibuild\apply.py 1.40.6 && qpm restore && qpm s build && qpm qmod manifest && scripts\createqmod.ps1 -QmodName HeartBeatQuest_1.40.6
+python multibuild\apply.py 1.40.4 && qpm restore && qpm s build && qpm qmod manifest && scripts\createqmod.ps1 -QmodName HeartBeatQuest_1.40.4
+python multibuild\apply.py 1.37.0 && qpm restore && qpm s build && qpm qmod manifest && scripts\createqmod.ps1 -QmodName HeartBeatQuest_1.37.0
